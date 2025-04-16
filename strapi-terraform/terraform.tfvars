29f821f2bea3f5d8cec4b35bab35bbd80046ba13
Prem Prakash Jena <prem@Prems-MacBook-Air.local>
2025-04-16 19:53:09 +0530

    Add .gitignore and exclude Terraform state files

--- strapi-terraform/terraform.tfvars
+++ strapi-terraform/terraform.tfvars
@@ -2,4 +2,4 @@ region         = "us-east-1"
 instance_type  = "t2.micro"
 ami            = "ami-0fc5d935ebf8bc3bc"
 key_name       = "strapi-key"  # Replace with your actual EC2 key pair name
-ecr_image_url  = "815454675511.dkr.ecr.us-east-1.amazonaws.com/strapi-app"
+ecr_image_url  = "123456789012.dkr.ecr.us-east-1.amazonaws.com/demo-repository"  # Demo ECR image URL
--- strapi-terraform/user-data.sh
+++ strapi-terraform/user-data.sh
@@ -9,10 +9,10 @@ systemctl start docker
 systemctl enable docker
 
 # Authenticate Docker with ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 815454675511.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin <aws_account_id>.dkr.ecr.us-east-1.amazonaws.com
 
 # Pull the Docker image from ECR
-docker pull 815454675511.dkr.ecr.us-east-1.amazonaws.com/strapi-app:latest
+docker pull <aws_account_id>.dkr.ecr.us-east-1.amazonaws.com/<repository_name>:latest
 
 # Run the container
-docker run -d -p 80:1337 --name strapi-container 815454675511.dkr.ecr.us-east-1.amazonaws.com/strapi-app:latest
+docker run -d -p 80:1337 --name strapi-container <aws_account_id>.dkr.ecr.us-east-1.amazonaws.com/<repository_name>:latest
